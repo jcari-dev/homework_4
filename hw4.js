2514f55f35e71752db2626d27ab297f1cf6c6a0b
jcari-rgb <582119jc@gmail.com>
2021-03-17 23:12:12 -0400

    objectception (woah new word) complete

---
+++ hw4.js
@@ -0,0 +1,19 @@
+const inception = { //oh, now I see why I never understood that movie, because my js knowledge was bad
+    reality: {
+        dreamLayer1: {
+            dreamLayer2: {
+                dreamLayer3: {
+                    dreamLayer4: {
+                        dreamLayer5: {
+                            dreamLayer6: {
+                                limbo: "Joseph Gordon Levitt"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null;
